de0db45c24b605db55f66d28d083ca8471d97843
jonzo99 <jonzojimenez@gmail.com>
2021-04-20 20:57:19 -0700

    I added the feature to zoom into the users location and updates user location

--- AppleTree/MapViewController.swift
+++ AppleTree/MapViewController.swift
@@ -12,7 +12,11 @@ class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    //let latitude = r.coordinates["latitude"]!
+   // let longitude = r.coordinates["longitude"]!
+    
     let locationManager = CLLocationManager()
+    let regionInMeters: Double = 10000
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +26,14 @@ class MapViewController: UIViewController {
     func setupLocationManager() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    // zooms into the users location
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate{
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
     }
     
     func checkLocationServices() {
@@ -37,6 +49,12 @@ class MapViewController: UIViewController {
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
             mapView.showsUserLocation = true  // this line of code shows blue dot for user location i can also go to map view and toggle it on and off
+            centerViewOnUserLocation()
+            // updates user location to show when user is moving
+            locationManager.startUpdatingLocation()
+            
+            
+            
             break
         case .denied:
             // show alert instructing them how to turn on permissions
@@ -56,10 +74,13 @@ class MapViewController: UIViewController {
 
 extension MapViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // we will be back
+        guard let location = locations.last else { return}
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mapView.setRegion(region, animated: true)
     }
-    
+    //whenever the authorization changes it runs through the switch statement
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        // Well be back
+        checkLocationAuthorization()
     }
 }
