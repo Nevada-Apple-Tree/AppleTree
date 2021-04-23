ad45d49543e76581780b16313627beb805e921cc
Michael King <mak3517@yahoo.com>
2021-04-23 10:29:25 -0700

    added code to the settings to be able to change user image

--- AppleTree/SettingsViewController.swift
+++ AppleTree/SettingsViewController.swift
@@ -8,8 +8,11 @@
 import UIKit
 import Parse
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var userImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,6 +30,31 @@ class SettingsViewController: UIViewController {
         sceneDelegate.window?.rootViewController = loginViewController
     }
     
+    
+    @IBAction func changeUserImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageAspectScaled(toFill: size)
+        
+        userImage.image = image
+        
+        dismiss(animated: true, completion: nil)
+    }
     
     /*
     // MARK: - Navigation
